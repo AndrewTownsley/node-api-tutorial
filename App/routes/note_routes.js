e87b6aad865487d6b288dcf6679fa1c7b94dd7ac
Andrew TOwnsley <atownsley86@gmail.com>
2021-10-16 21:15:25 -0600

    Working on bug == invalid schema, expected mongodb==

--- App/routes/note_routes.js
+++ App/routes/note_routes.js
@@ -1,6 +1,13 @@
 module.exports = function(app, db) {
-    app.post('./notes', (req, res) => {
-        // You will create your note here...
-        res.send('Hello');
-    })
-}
+    const collection = 
+    app.post('/notes', (req, res) => {
+      const note = { text: req.body.body, title: req.body.title };
+      db.collection('notes').insert(note, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+    });
+  };
