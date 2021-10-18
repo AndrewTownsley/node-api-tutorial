05fcae4d454c503cc7af9b5e3d343b1b38c201e3
Andrew TOwnsley <atownsley86@gmail.com>
2021-10-17 20:46:37 -0600

    test 123

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
 const port = 8000;
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));MongoClient.connect(db.url, (err, database) => {
